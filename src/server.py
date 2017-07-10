9dca04645e6e8b2c62f29b5deff015c6015452e7
Chen, Guo <guo.chen@polycom.com>
2017-07-10 19:27:22 +0800

    add a web server to receive post image data

--- src/server.py
+++ src/server.py
@@ -1,16 +1,28 @@
 import tornado.ioloop
 import tornado.web
+import mnist_loader
+import network
+import json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
+class PostImageArray(tornado.web.RequestHandler):
+    def post(self):
+        imgArray = self.get_argument('image')
+
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/image", PostImageArray),
     ])
 
 if __name__ == "__main__":
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper();
+    net = network.Network([784, 30, 10])
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
